e7a1f4f60888743c8f3fe4494a6fc66001d07d27
machinelearning4health <75764066+machinelearning4health@users.noreply.github.com>
2021-07-01 20:31:35 -0400

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,39 @@
+python grn.py -elr 0.0001 -dlr 0.0001
+python grnS.py -elr 0.0001 -dlr 0.0001
+python grn.py -k 3 --save_dir ./saved_models/grn-3/ -elr 0.0001 -dlr 0.0001
+python grnS.py -k 3 --save_dir ./saved_models/grns-3/ -elr 0.0001 -dlr 0.0001
+
+python gruself_grn.py -elr 0.0001 -dlr 0.0001
+python gruself_grns.py -elr 0.0001 -dlr 0.0001
+python gruself_grn.py -k 3 --save_dir ./saved_models/gruself_grn-3/ -elr 0.0001 -dlr 0.0001
+python gruself_grns.py -k 3 --save_dir ./saved_models/gruself_grns-3/ -elr 0.0001 -dlr 0.0001
+
+python hita_grn.py -elr 0.0001 -dlr 0.0001
+python hita_grns.py -elr 0.0001 -dlr 0.0001
+python hita_grn.py -k 3 --save_dir ./saved_models/hita_grn-3/ -elr 0.0001 -dlr 0.0001
+python hita_grns.py -k 3 --save_dir ./saved_models/hita_grns-3/ -elr 0.0001 -dlr 0.0001
+
+python lsan_grn.py -elr 0.0001 -dlr 0.0001
+python lsan_grns.py -elr 0.0001 -dlr 0.0001
+python lsan_grn.py -k 3 --save_dir ./saved_models/lsan_grn-3/ -elr 0.0001 -dlr 0.0001
+python lsan_grns.py -k 3 --save_dir ./saved_models/lsan_grns-3/ -elr 0.0001 -dlr 0.0001
+
+python retain_grn.py -elr 0.0001 -dlr 0.0001
+python retain_grns.py -elr 0.0001 -dlr 0.0001
+python retain_grn.py -k 3 --save_dir ./saved_models/retain_grn-3/ -elr 0.0001 -dlr 0.0001
+python retain_grns.py -k 3 --save_dir ./saved_models/retain_grns-3/ -elr 0.0001 -dlr 0.0001
+
+python retainEx_grn.py -elr 0.0001 -dlr 0.0001
+python retainEx_grns.py -elr 0.0001 -dlr 0.0001
+python retainEx_grn.py -k 3 --save_dir ./saved_models/retainEx_grn-3/ -elr 0.0001 -dlr 0.0001
+python retainEx_grns.py -k 3 --save_dir ./saved_models/retainEx_grns-3/ -elr 0.0001 -dlr 0.0001
+
+python sand_grn.py -elr 0.0001 -dlr 0.0001
+python sand_grns.py -elr 0.0001 -dlr 0.0001
+python sand_grn.py -k 3 --save_dir ./saved_models/sand_grn-3/ -elr 0.0001 -dlr 0.0001
+python sand_grns.py -k 3 --save_dir ./saved_models/sand_grns-3/ -elr 0.0001 -dlr 0.0001
+
+python timeline_grn.py -elr 0.0001 -dlr 0.0001
+python timeline_grns.py -elr 0.0001 -dlr 0.0001
+python timeline_grn.py -k 3 --save_dir ./saved_models/timeline_grn-3/ -elr 0.0001 -dlr 0.0001
+python timeline_grns.py -k 3 --save_dir ./saved_models/timeline_grns-3/ -elr 0.0001 -dlr 0.0001
